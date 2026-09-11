c1657c4da82c7796e8b3847f89eb53d84b7ddae8
noelms25 <noelms25.8.3@gmail.com>
2026-09-11 20:14:46 +0200

    Subo cambios

--- app/config.py
+++ app/config.py
@@ -1,13 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    # Ruta de la base de datos
-    BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    DATABASE = os.path.join(BASE_DIR, "BBDD.db")
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', '8f1d9c7a2b4e6f0a9d3c5b7e1f4a6c8d')
 
-    # Flask
-    DEBUG = True
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+    DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
     # Opcional
     JSON_SORT_KEYS = False
