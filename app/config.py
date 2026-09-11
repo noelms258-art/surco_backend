5b5a39e8e9f8e74b75c4d2e5a86ff4429531b384
noelms25 <noelms25.8.3@gmail.com>
2026-09-11 20:17:47 +0200

    Subo cambios

--- app/config.py
+++ app/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', '8f1d9c7a2b4e6f0a9d3c5b7e1f4a6c8d')
 
     DATABASE_URL = os.getenv("DATABASE_URL")
 
